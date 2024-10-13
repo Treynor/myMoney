c830eb7d94da750a25b5fdccf1bbc031cd3d78dc
Tony Reynolds <treyno@gmail.com>
2024-10-13 22:02:41 +0100

    updated to v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, useNavigate, Routes } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
 import Home from './pages/home/Home'
@@ -7,22 +8,28 @@ import Signup from './pages/signup/Signup'
 import Navbar from './components/Navbar'
 
 function App() {
+
+  const { authIsReady, user  } = useAuthContext()
+  const navigate = useNavigate()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      { authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/"
+            element={(!user && <Navigate to="/login" />) || (user && <Home />)}>
+            </Route>
+            <Route path="/login"
+            element={(user && <Navigate to="/" />) || (!user && <Login />) }>
+            </Route>
+            <Route path="/signup"
+            element={(user && <Navigate to="/" />) || (!user && <Signup />)}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
